07fc382b27004057210ac0e77375ebc81ce66531
ashbob999 <ashbob999@gmail.com>
2020-05-11 11:41:45 +0100

    Added Copyright License header to all classes.

--- src/main/java/com/ashbob999/gameEngine2d/core/WindowFlag.java
+++ src/main/java/com/ashbob999/gameEngine2d/core/WindowFlag.java
@@ -4,6 +4,23 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * 
+ * <pre>
+ * Copyright 2020 ashbob999
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * </pre>
+ * 
  * Window Flags:
  * <ul>
  * <li>{@link #RESIZEABLE} - is the window resizable</li>
@@ -13,9 +30,9 @@ import java.util.Map;
  * <li>{@link #MAX_FPS} -lets FPS go over the target FPS (Doesn't work is vSync is enabled)</li>
  * <li>{@link #ANTIALIASING} -</li>
  * </ul>
- * 
+ *
  * @author ashbob999
- * 
+ *
  */
 public enum WindowFlag {
 	/**
--- src/main/java/com/ashbob999/gameEngine2d/graphics/Mesh.java
+++ src/main/java/com/ashbob999/gameEngine2d/graphics/Mesh.java
@@ -30,6 +30,27 @@ import java.util.List;
 import org.joml.Vector3f;
 import org.lwjgl.system.MemoryUtil;
 
+/**
+ * 
+ * <pre>
+ * Copyright 2020 ashbob999
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * </pre>
+ *
+ * @author ashbob999
+ *
+ */
 public class Mesh {
 
 	private static final Vector3f DEFAULT_COLOUR = new Vector3f(1.0f, 1.0f, 1.0f);
